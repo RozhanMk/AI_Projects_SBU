ae6f85641aff606a99bf07b6141b9bd7f05456b5
RozhanMk <rozhan.mirzaei82@gmail.com>
2022-09-07 20:59:08 +0430

    added comments

--- sentence_generator.py
+++ sentence_generator.py
@@ -4,7 +4,7 @@ import random
 
 first_words = set()
 ordered_words = []
-words_pairs = dict()
+words_pairs = {}
 
 with codecs.open("speech.txt", mode="r", encoding='utf-8') as f:
     words = f.read().split(" ")
@@ -22,7 +22,7 @@ with codecs.open("speech.txt", mode="r", encoding='utf-8') as f:
                 first_words.add(sample.strip())
             #putting words and their pairs in a dictionary
             if sample.strip() not in words_pairs.keys():
-                words_pairs[sample.strip()] = dict()
+                words_pairs[sample.strip()] = {}
             if i>0:
                 previous_word = ordered_words[i-1].strip()
                 if previous_word in words_pairs.keys():
@@ -30,31 +30,26 @@ with codecs.open("speech.txt", mode="r", encoding='utf-8') as f:
                         words_pairs[previous_word][sample.strip()] = 1
                     else:
                         words_pairs[previous_word][sample.strip()] +=1
-
             i+=1
 
 def get_sentence():
     result = []
-    first_word = random.choice(list(first_words))
+    first_word = random.choice(list(first_words)) #get a random first word
     result.append(first_word)
     
-
     while "." not in result[-1]:
-        
         last_word = result[-1]
         chances = []
         for key in words_pairs[last_word].keys():
             chances.append(words_pairs[last_word][key])
-
-        next_word = random.choices( list(words_pairs[last_word]) , weights = chances, k=1)
+        next_word = random.choices( list(words_pairs[last_word]) , weights = chances, k=1) #get a random next word according to the chances
         result.append(next_word[0])
-
-    return ' '.join(result)
+    return " ".join(result)
         
 
-with open("out.txt", "w") as f:
+#generate 50 sentences
+with open("output.txt", "w") as f:
     result = ""
     for i in range(50):
-        result += get_sentence()
-        result += "\n"
+        result += get_sentence() + "\n"
     f.write(result)
